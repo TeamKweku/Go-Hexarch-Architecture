7e43e87efd1ff4ebc7a5f5502f88b8adbb1b9657
teamkweku <teamkweku@outlook.com>
2024-09-02 08:49:51 +0000

    fix(golangci-lint): unwrapped error fix in auth service

--- internal/core/application/auth/auth_service.go
+++ internal/core/application/auth/auth_service.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"fmt"
+
 	"github.com/teamkweku/code-odessey-hex-arch/config"
 	"github.com/teamkweku/code-odessey-hex-arch/internal/core/domain/auth"
 	"github.com/teamkweku/code-odessey-hex-arch/internal/core/domain/user"
@@ -19,9 +21,19 @@ func (as *AuthService) CreateToken(
 	user *user.User,
 	cfg config.Config,
 ) (string, *auth.Payload, error) {
-	return as.tokenService.CreateToken(user, cfg)
+	token, payload, err := as.tokenService.CreateToken(user, cfg)
+	if err != nil {
+		return "", nil, fmt.Errorf("auth service - create token: %w", err)
+	}
+
+	return token, payload, nil
 }
 
 func (as *AuthService) VerifyToken(token string) (*auth.Payload, error) {
-	return as.tokenService.VerifyToken(token)
+	payload, err := as.tokenService.VerifyToken(token)
+	if err != nil {
+		return nil, fmt.Errorf("auth service - verify token: %w", err)
+	}
+
+	return payload, nil
 }
