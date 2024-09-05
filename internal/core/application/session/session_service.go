7646e199c8624820e857d27a91e295d965e49b5c
teamkweku <teamkweku@outlook.com>
2024-09-05 13:35:19 +0000

    fix(golangci-lint): unwrapped error fix in session service

--- internal/core/application/session/session_service.go
+++ internal/core/application/session/session_service.go
@@ -2,6 +2,7 @@ package session
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/google/uuid"
 	"github.com/teamkweku/code-odessey-hex-arch/internal/core/domain/auth"
@@ -17,17 +18,35 @@ func NewSessionService(sessionRepo outbound.SessionRepository) *SessionService {
 }
 
 func (s *SessionService) CreateSession(ctx context.Context, session *auth.Sessions) error {
-	return s.sessionRepo.CreateSession(ctx, session)
+	if err := s.sessionRepo.CreateSession(ctx, session); err != nil {
+		return fmt.Errorf("failed to create session: %w", err)
+	}
+
+	return nil
 }
 
 func (s *SessionService) GetSession(ctx context.Context, id uuid.UUID) (*auth.Sessions, error) {
-	return s.sessionRepo.GetSession(ctx, id)
+	session, err := s.sessionRepo.GetSession(ctx, id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get session: %w", err)
+	}
+
+	return session, nil
 }
 
 func (s *SessionService) GetSessionByUserID(ctx context.Context, userID uuid.UUID) (*auth.Sessions, error) {
-	return s.sessionRepo.GetSessionByUserID(ctx, userID)
+	session, err := s.sessionRepo.GetSessionByUserID(ctx, userID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get session: %w", err)
+	}
+
+	return session, nil
 }
 
 func (s *SessionService) DeleteSession(ctx context.Context, id uuid.UUID) error {
-	return s.sessionRepo.DeleteSession(ctx, id)
+	if err := s.sessionRepo.DeleteSession(ctx, id); err != nil {
+		return fmt.Errorf("failed to delete session: %w", err)
+	}
+
+	return nil
 }
